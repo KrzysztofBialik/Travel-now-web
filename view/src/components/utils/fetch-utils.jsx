59e265cf49a99ce389de2e525b8afbdc6a8b656e
KrzysztofBialik <105109608+KrzysztofBialik@users.noreply.github.com>
2022-11-21 23:13:56 +0100

    integration introduction

---
+++ view/src/components/utils/fetch-utils.jsx
@@ -0,0 +1,83 @@
+export const URL_PREFIX = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : '';
+
+const checkForError = async (response) => {
+    if (!response.ok) {
+        if (response.headers.get('content-type') === 'application/json') {
+            const json = await response.json()
+            throw new Error(json.message)
+        }
+        throw new Error("Something went wrong")
+    }
+    return response
+}
+
+export const doGet = (endpoint) => {
+    const headers = addAuthorizationHeader({
+        'Accept': 'application/json',
+    })
+
+    const url = endpoint.startsWith("http") ? endpoint : URL_PREFIX + endpoint;
+
+    return fetch(url, {
+        method: 'GET',
+        headers: headers,
+    })
+        .then(response => checkForError(response))
+}
+
+export const doPost = (endpoint, body = {}, authorize = true) => {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
+
+    if (authorize) {
+        headers = addAuthorizationHeader(headers)
+    }
+
+    return fetch(URL_PREFIX + endpoint, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(body)
+    })
+        .then(response => checkForError(response))
+}
+
+export const doPatch = (endpoint, body = {}) => {
+    const headers = addAuthorizationHeader({
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    })
+
+    return fetch(URL_PREFIX + endpoint, {
+        method: 'PATCH',
+        headers: headers,
+        body: JSON.stringify(body)
+    })
+        .then(response => checkForError(response))
+}
+
+export const doDelete = (endpoint) => {
+    const headers = addAuthorizationHeader({
+        'Accept': 'application/json',
+    })
+
+    return fetch(URL_PREFIX + endpoint, {
+        method: 'DELETE',
+        headers: headers,
+    })
+        .then(response => checkForError(response))
+}
+
+const addAuthorizationHeader = (headers) => {
+    const jwt = localStorage.getItem("ACCESS_TOKEN")
+
+    if (jwt) {
+        return {
+            ...headers,
+            Authorization: `Bearer ${jwt}`,
+        }
+    }
+
+    return headers
+}
