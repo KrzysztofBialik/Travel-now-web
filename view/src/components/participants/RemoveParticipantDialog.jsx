3f8e4aa044dee4e405324c9c14039df6449a34fc
DorianBestrzynski <80275992+DorianBestrzynski@users.noreply.github.com>
2022-12-04 10:28:36 +0100

    First changes

--- view/src/components/participants/RemoveParticipantDialog.jsx
+++ view/src/components/participants/RemoveParticipantDialog.jsx
@@ -9,27 +9,59 @@ import { DialogTitle } from '@mui/material';
 
 import { SuccessToast } from '../toasts/SuccessToast';
 import { ErrorToast } from '../toasts/ErrorToast';
+import { DeletingPermissionApiErrorToast } from '../toasts/DeletingPermissionApiErrorToast';
+import { doDelete } from "../../components/utils/fetch-utils";
 
-export const RemoveParticipantDialog = ({ open, onClose }) => {
+export const RemoveParticipantDialog = ({ open, onClose, groupId, userId }) => {
 
     const [successToastOpen, setSuccessToastOpen] = useState(false);
     const [errorToastOpen, setErrorToastOpen] = useState(false);
+    const [apiErrorToastOpen, setApiErrorToastOpen] = useState(false);
+    const [apiErrorMessage, setApiErrorMessage] = useState("deafukt")
+    const [hasFailed, setHasFailed] = useState(false)
 
-    const handleSuccessClose = () => {
+    const handleSuccessClose = async () => {
+    
+        await deleteUserFromGroup()
+        console.log("Has failed" + hasFailed)
+        console.log("Interesting " + apiErrorMessage)
+        if(hasFailed){
+         console.log("Interesting " + apiErrorMessage)
+         setApiErrorToastOpen(true)
+         onClose()
+        }
+        else {
+        // window.location.reload(false);
         setSuccessToastOpen(true);
         onClose();
+        }
+        
     };
 
     const handleErrorClose = () => {
+        console.log("here");
         setErrorToastOpen(true);
         onClose();
     };
+    localStorage.setItem("ACCESS_TOKEN", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjMxLCJ1c2VybmFtZSI6IkRvcmlhbiJ9.spFruljGVOCA2_CVdl4nP36AcWeKy2YvEIQ5aYoqrxw")
+    localStorage.setItem("userId", 31)
+
+    const deleteUserFromGroup = async () => {
+        await doDelete('/api/v1/trip-group/coordinator-user?' + new URLSearchParams({ groupId: groupId ,userId: userId}).toString())
+        .then(response => response.json())
+        .catch(err => {
+            setHasFailed(true)
+            setApiErrorMessage(err);
+            console.log("doring good job" + hasFailed + apiErrorMessage)
+        });
+
+    }
 
     return (
         <div>
             <SuccessToast open={successToastOpen} onClose={() => setSuccessToastOpen(false)} message="Participant successfully removed from group." />
             <ErrorToast open={errorToastOpen} onClose={() => setErrorToastOpen(false)} message="Ups! Something went wrong. Try again." />
-
+            <DeletingPermissionApiErrorToast open={apiErrorToastOpen} onClose={() => setApiErrorToastOpen(false)} message={apiErrorMessage} />
             <Dialog
                 open={open}
                 onClose={onClose}
